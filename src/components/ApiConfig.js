e1910620ca6c0feb8eb4c72d6dc1dbe5d1a52626
pauloandredm <pauloadmendonca@gmail.com>
2023-07-03 08:44:00 -0300

    tirando espaço
    adadad

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,4 +1,4 @@
 // apiConfig.js
-const API_BASE_URL = 'http://c0ee-200-164-157-66.ngrok-free.app ';
+const API_BASE_URL = 'http://c0ee-200-164-157-66.ngrok-free.app';
 
 export default API_BASE_URL;
