58797aa3033a19e36e3405ba87299612dbe3d7dc
pauloandredm <pauloadmendonca@gmail.com>
2023-11-27 08:32:27 -0300

    up url
    aaaa

--- src/components/ApiConfig.js
+++ src/components/ApiConfig.js
@@ -1,6 +1,6 @@
 // apiConfig.js
 /* const API_BASE_URL = 'http://localhost:8000'; */
-const API_BASE_URL = 'https://0d59-200-164-157-66.ngrok-free.app';
+const API_BASE_URL = 'https://d001-201-20-70-162.ngrok-free.app';
 
 
 export default API_BASE_URL;
