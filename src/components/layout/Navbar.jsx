0ee331c09365f7f818d7b149508203a02478c1a7
pauloandredm <pauloadmendonca@gmail.com>
2024-05-29 08:40:53 -0300

    up

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -83,9 +83,9 @@ function Navbar() {
                 <li className={styles.item}><Link to="/avaliacao">Avaliação</Link></li>
               ) : null}
 
-              {inAvaliacao && <li className={styles.item}><Link to="/AcordoAceitoDesempenho">Ver Acordo Desempenho</Link></li>}
+              {inAvaliacao && <li className={styles.item}><Link to="/AcordoAceitoDesempenho">Ver Acordo de Desempenho</Link></li>}
 
-              {chefia && <li className={styles.item}><Link to="/acordo-desempenho">Acordo Desempenho</Link></li>}
+              {chefia && <li className={styles.item}><Link to="/acordo-desempenho">Acordo de Desempenho</Link></li>}
 
               {gestor || !inAvaliacao ? (
                 <li className={styles.item}><Link to="/perfil">Resumo avaliação</Link></li>
