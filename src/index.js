49c3827c239f5933acf37882e87e80539c7c5798
pauloandredm <pauloadmendonca@gmail.com>
2024-08-02 09:29:39 -0300

    aaaaaaaaaah

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from "./AuthContext";
@@ -7,12 +7,11 @@ import setupAxiosInterceptors2 from './axiosConfig';
 
 setupAxiosInterceptors2();
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
